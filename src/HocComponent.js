12595987907aa357095dcec24e0705bfbc46ebc2
itenl <nesa.wong@gmail.com>
2021-07-08 11:57:24 +0800

    added comment

--- src/HocComponent.js
+++ src/HocComponent.js
@@ -11,6 +11,7 @@ export default (WrappedComponent, getRef) => {
       return (
         <WrappedComponent
           ref={comp => {
+            // 在使用RN release打包后在sticky获取到的screenContext为HOC上下文，需健全区分__HOCNAME__
             if (comp && !comp.__HOCNAME__) comp && getRef && getRef(comp);
           }}
           {...this.props}
